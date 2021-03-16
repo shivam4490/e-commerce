dd3c4e2d3356934f5a52753a30cb78e1980aeddc
shivam4490 <csg9152@gmail.com>
2021-03-17 00:26:48 +0530

    rating component created

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,29 @@
 import React from 'react'
-import { Navbar, Nav} from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {
-    return (
-        <header>
-           <Navbar bg="dark" varient='dark' expand="lg" collapseOnSelect>
-  <Navbar.Brand href="/">YourShop</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="ml-auto">
-      <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-      <Nav.Link href="/login"><i className='fas fa-user'></i>Sign In</Nav.Link>
-      
-    </Nav>
-    
-  </Navbar.Collapse>
-</Navbar>
-        </header>
-    )
+  return (
+    <header>
+      <Navbar bg='dark' varient='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/' className='text-light'>
+            E-Shop
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <Nav.Link href='/cart' className='text-light'>
+                <i className='fas fa-shopping-cart'></i>Cart
+              </Nav.Link>
+              <Nav.Link href='/login' className='text-light'>
+                <i className='fas fa-user'></i>Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
 }
 
 export default Header
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,16 +1,35 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 import products from '../products'
+import Rating from '../components/Rating'
 
-const Product = ( { product} ) => {
-    return (
-        <>
-            <Card className='my-3 p-3 rounded'>
-                <a href={`/product/${product._id}`}></a>
-                <Card.Img src={product.image} variant='top' />
-            </Card>   
-        </>
-    )
+const Product = ({ product }) => {
+  return (
+    <>
+      <Card className='my-3 p-3 rounded'>
+        <a href={`/product/${product._id}`}></a>
+        <Card.Img src={product.image} variant='top' />
+
+        <Card.Body>
+          <a href={`/product/${product._id}`}></a>
+          <Card.Title as='div'>
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Card.Body>
+
+        <Card.Text as='div' style={{ marginLeft: 15 }}>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
+
+        <Card.Text as='h3' className='p-3'>
+          ${product.price}
+        </Card.Text>
+      </Card>
+    </>
+  )
 }
 
 export default Product
