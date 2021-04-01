9ccc0454682ffd1d6e197a6878093b302e66f864
shivam4490 <csg9152@gmail.com>
2021-04-01 14:42:21 +0530

    add to cart function

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -8,7 +8,7 @@ const Loader = () => {
       role='status'
       style={{ width: 100, height: 100, margin: 'auto', display: 'block' }}
     >
-      <span class='sr-only'>Loading... </span>
+      <span className='sr-only'>Loading... </span>
     </Spinner>
   )
 }
