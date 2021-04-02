58d4a81ccfe61da87428fe10ddbab7804f0238af
shivam4490 <csg9152@gmail.com>
2021-04-02 16:56:27 +0530

    protected auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,4 +23,20 @@ const authUser = async (req, res) => {
   }
 }
 
-module.exports.authUser = authUser
+//GET /api/users/profile
+const getUserProfile = async (req, res) => {
+  const user = await User.findById(req.user._id)
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+}
+
+module.exports = { authUser, getUserProfile }
