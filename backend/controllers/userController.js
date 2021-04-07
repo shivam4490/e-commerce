cf542c9415cbde0e0d6dbad3a4c2d311c35e44e6
shivam4490 <csg9152@gmail.com>
2021-04-07 22:42:48 +0530

    admin middlewares and user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -96,4 +96,34 @@ const updateUserProfile = async (req, res) => {
   }
 }
 
-module.exports = { authUser, getUserProfile, registerUser, updateUserProfile }
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private/Admin
+const getUsers = async (req, res) => {
+  const users = await User.find({})
+  res.json(users)
+}
+
+// @desc    Delete user
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
+const deleteUser = async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    await user.remove()
+    res.json({ message: 'User removed' })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+}
+
+module.exports = {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+}
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -5,10 +5,12 @@ const {
   getUserProfile,
   registerUser,
   updateUserProfile,
+  getUsers,
+  deleteUser,
 } = require('../controllers/userController')
-const { protect } = require('../middleware/authMiddleware')
+const { protect, admin } = require('../middleware/authMiddleware')
 
-router.route('/').post(registerUser)
+router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/login', authUser)
 router
   .route('/profile')
