c711d4668e94401bb84ae4ff7270e222e9308749
shivam4490 <csg9152@gmail.com>
2021-04-08 13:28:18 +0530

    edit user done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,7 +33,7 @@ function App() {
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
-          <Route path='/admin/userlist/:id/edit' component={UserEditScreen} />
+          <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
